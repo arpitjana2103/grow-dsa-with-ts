57b4d2f3c8bcabbb2df98077d2f72afb9f825168
Arpit Jana <106902674+arpitjana2103@users.noreply.github.com>
2026-05-08 19:36:31 +0530

    Time: 6 ms (16.18%) | Memory: 58.6 MB (11.53%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.ts
@@ -0,0 +1,40 @@
+function reverse(str, left, right){
+    while(left < right){
+        let temp = str[left];
+        str[left] = str[right];
+        str[right] = temp;
+        left++;
+        right--;
+    }
+}
+
+
+
+function reverseWords(s: string): string {
+    const str = s.trim().split('');
+
+    const n = str.length;
+    let slow = 0;
+    let fast = 0;
+
+    reverse(str, 0, n-1);
+
+    while(fast < n){
+       while(fast < n && str[fast] === ' ') fast++;
+
+       const revL = slow;
+
+       while(fast < n && str[fast] !== ' '){
+        str[slow++] = str[fast++];
+       }
+
+       const revR = slow-1;
+
+       reverse(str, revL, revR);
+
+       str[slow++] = ' ';
+    }
+    
+    console.log(slow);
+    return str.slice(0, slow-1).join('');
+};
